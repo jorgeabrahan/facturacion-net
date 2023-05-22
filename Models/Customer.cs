fd8ef63649b403e85b09a21a2892d6cb0e0c4e4e
jorgeabrahan <jorge24abrahan@gmail.com>
2023-05-22 16:43:02 -0600

    Create customer model to store information about the buyer

---
+++ Models/Customer.cs
@@ -0,0 +1,33 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace facturacion.Models;
+/* Cliente */
+public class Customer
+{
+  [Key]
+  public Guid CustomerId { get; set; }
+
+  [Required]
+  [MaxLength(250)]
+  public String? Name { get; set; }
+
+  [MaxLength(250)]
+  public String? Address { get; set; }
+
+  [Required]
+  [MaxLength(250)]
+  public String? RTN { get; set; }
+
+  [Required]
+  [MaxLength(250)]
+  public String? PhoneNumber { get; set; }
+
+  public CustomerTypes CustomerType { get; set; }
+}
+
+public enum CustomerTypes
+{
+  Normal,
+  Wholesale,
+  Internal
+}
