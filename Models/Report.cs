88a803e55a79ff22f11eb6be49d5523d1446204f
jorgeabrahan <jorge24abrahan@gmail.com>
2023-05-22 23:29:41 -0600

    Create method to filter invoices by created date

--- Models/Report.cs
+++ Models/Report.cs
@@ -22,5 +22,17 @@ public class Report
   [MaxLength(250)]
   public String? Content { get; set; }
 
+  [Required]
+  public float TotalBills { get; set; }
+
   public virtual ICollection<Invoice>? Invoices { get; set; }
+
+  public IEnumerable<Invoice> FilterInvoices(DateTime startDate, DateTime endDate)
+  {
+    if (Invoices == null)
+    {
+      return Enumerable.Empty<Invoice>();
+    }
+    return Invoices.Where(invoice => invoice.CreationDate >= startDate && invoice.CreationDate <= endDate);
+  }
 }
