b9a67369423d3e33378fae38ae575b52feeefa5a
jorgeabrahan <jorge24abrahan@gmail.com>
2023-05-25 00:34:08 -0600

    Rename context and add users model to context

--- Context.cs
+++ Context.cs
@@ -1,19 +1,17 @@
 
-using facturacion.Models;
 using Microsoft.EntityFrameworkCore;
+using facturacion.Models;
 
+namespace facturacion;
 
-public class Context:DbContext {
-public DbSet<Article> Articles{get;set;}
+public class FacturacionContext : DbContext
+{
+  public DbSet<User>? Users { get; set; }
+  public DbSet<Article>? Articles { get; set; }
+  public DbSet<Customer>? Customers { get; set; }
+  public DbSet<Inventory>? Inventorys { get; set; }
+  public DbSet<Invoice>? Invoices { get; set; }
+  public DbSet<Product>? Products { get; set; }
 
-public DbSet<Customer> Customers{get;set;}
-public DbSet<Inventory> Inventorys{get;set;}
-public DbSet<Invoice> Invoices{get;set;}
-public DbSet<Product> Products{get;set;}
-public DbSet<Report> Reports{get;set;}
-public DbSet<User> Users{get;set;}
-
-
-public Context(DbContextOptions<Context> options) : base(options){}
-
+  public FacturacionContext(DbContextOptions<FacturacionContext> options) : base(options) { }
 }
