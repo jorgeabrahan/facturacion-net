f58faf681f2b3a0872740208866bfffc58e9bb61
jorgeabrahan <jorge24abrahan@gmail.com>
2023-05-25 00:35:09 -0600

    Fix warning and rename properties

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -1,52 +1,43 @@
 using facturacion.Models;
+namespace facturacion.Services;
 
 public class UserService : IUserService
 {
-    //Inyeccion de dependencias.
-    Context context;
-    public UserService(Context DbContext) => context = DbContext;
+  //Inyeccion de dependencias.
+  FacturacionContext context;
+  public UserService(FacturacionContext dbContext) => context = dbContext;
 
-    //CRUD
-    //create
-    public async Task Create(User newinvoice)
-    {
-        await context.AddAsync(newinvoice);
-        await context.SaveChangesAsync();
-    }
-    //Read
-    public IEnumerable<User> Read() => context.Users;
-    //update
-    public async Task Update(Guid id, User UpdateUser)
-    {
-        var addNew = context.Users.Find(id);
-        if (addNew == null) return;
-        
-            addNew.Username = UpdateUser.Username;
-            addNew.Password = UpdateUser.Password;
-         
-            await context.SaveChangesAsync();
-        
-    }
+  public async Task Create(User user)
+  {
+    user.UserId = Guid.NewGuid();
+    await context.AddAsync<User>(user);
+    await context.SaveChangesAsync();
+  }
 
-    //Delelte
+  public IEnumerable<User>? Read() => context.Users;
 
-    public async Task Delete(Guid id)
-    {
-        var test = context.Users.Find(id);
-        if (test != null)
-        {
-            context.Remove(test);
-            await context.SaveChangesAsync();
-        }
-    }
+  public async Task Update(Guid id, User updated)
+  {
+    var user = context.Users?.Find(id);
+    if (user == null) return;
+    user.Username = updated.Username;
+    user.Password = updated.Password;
+    await context.SaveChangesAsync();
+  }
 
+  public async Task Delete(Guid id)
+  {
+    var user = context.Users?.Find(id);
+    if (user == null) return;
+    context.Remove(user);
+    await context.SaveChangesAsync();
+  }
 }
 
 public interface IUserService
 {
-    Task Create(User newinvoice);
-    IEnumerable<User> Read();
-    Task Update(Guid id, User upinvoice);
-    Task Delete(Guid id);
-
+  Task Create(User user);
+  IEnumerable<User>? Read();
+  Task Update(Guid id, User updated);
+  Task Delete(Guid id);
 }
