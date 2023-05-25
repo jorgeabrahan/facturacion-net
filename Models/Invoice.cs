5c99dfc4a4aefc56161c210569088a056b7a00a7
jorgeabrahan <jorge24abrahan@gmail.com>
2023-05-24 22:35:58 -0600

    Create invoice new properties to calculate total

--- Models/Invoice.cs
+++ Models/Invoice.cs
@@ -15,7 +15,8 @@ public class Invoice
   [ForeignKey("CustomerId")]
   public Guid CustomerId { get; set; }
 
-  [Required]
+  public float SubTotal { get; set; }
+  public float ISV { get; set; }
   public float Total { get; set; }
 
   public DateTime CreationDate { get; set; }
@@ -25,6 +26,43 @@ public class Invoice
 
   public Invoice()
   {
+    SubTotal = 0;
+    ISV = 0;
+    Total = 0;
+    /* Set creation date when an invoice is created */
+    CreationDate = DateTime.Now;
+  }
+}
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace facturacion.Models;
+/* Factura */
+public class Invoice
+{
+  /*
+  An invoice belongs to a customer
+  An invoice has many products
+  */
+  [Key]
+  public Guid InvoiceId { get; set; }
+
+  [ForeignKey("CustomerId")]
+  public Guid CustomerId { get; set; }
+
+  public float SubTotal { get; set; }
+  public float ISV { get; set; }
+  public float Total { get; set; }
+
+  public DateTime CreationDate { get; set; }
+
+  public virtual Customer? Customer { get; set; }
+  public virtual ICollection<Product>? Products { get; set; }
+
+  public Invoice()
+  {
+    SubTotal = 0;
+    ISV = 0;
     Total = 0;
     /* Set creation date when an invoice is created */
     CreationDate = DateTime.Now;
