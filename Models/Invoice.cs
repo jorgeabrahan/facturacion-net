ea4e16c38e14b9b7d472df5d06084eec45434557
jorgeabrahan <jorge24abrahan@gmail.com>
2023-05-24 22:40:36 -0600

    Fix invoice duplicate code

--- Models/Invoice.cs
+++ Models/Invoice.cs
@@ -33,38 +33,3 @@ public class Invoice
     CreationDate = DateTime.Now;
   }
 }
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
-namespace facturacion.Models;
-/* Factura */
-public class Invoice
-{
-  /*
-  An invoice belongs to a customer
-  An invoice has many products
-  */
-  [Key]
-  public Guid InvoiceId { get; set; }
-
-  [ForeignKey("CustomerId")]
-  public Guid CustomerId { get; set; }
-
-  public float SubTotal { get; set; }
-  public float ISV { get; set; }
-  public float Total { get; set; }
-
-  public DateTime CreationDate { get; set; }
-
-  public virtual Customer? Customer { get; set; }
-  public virtual ICollection<Product>? Products { get; set; }
-
-  public Invoice()
-  {
-    SubTotal = 0;
-    ISV = 0;
-    Total = 0;
-    /* Set creation date when an invoice is created */
-    CreationDate = DateTime.Now;
-  }
-}
