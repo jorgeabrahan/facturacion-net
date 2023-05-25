e18b68dd02a6c757aeffa313306b35252789b1de
jorgeabrahan <jorge24abrahan@gmail.com>
2023-05-25 00:42:58 -0600

    Reorder models from context and add Report to context

--- Context.cs
+++ Context.cs
@@ -6,12 +6,13 @@ namespace facturacion;
 
 public class FacturacionContext : DbContext
 {
-  public DbSet<User>? Users { get; set; }
   public DbSet<Article>? Articles { get; set; }
   public DbSet<Customer>? Customers { get; set; }
-  public DbSet<Inventory>? Inventorys { get; set; }
+  public DbSet<Inventory>? Inventories { get; set; }
   public DbSet<Invoice>? Invoices { get; set; }
   public DbSet<Product>? Products { get; set; }
+  public DbSet<Report>? Reports { get; set; }
+  public DbSet<User>? Users { get; set; }
 
   public FacturacionContext(DbContextOptions<FacturacionContext> options) : base(options) { }
 }
