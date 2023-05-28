8ba34f95155db034230afec0ad1f0fdc4dd20045
jorgeabrahan <jorge24abrahan@gmail.com>
2023-05-28 00:17:12 -0600

    Add scoped services and sql server to program file

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using facturacion;
+using facturacion.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,14 +9,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSqlServer<FacturacionContext>("Data Source=localhost; Initial Catalog=facturationDB;user id=sa;password=Programaci0n$;Encrypt=False");
+builder.Services.AddScoped<IArticleService, ArticleService>();
+builder.Services.AddScoped<ICustomerService, CustomerService>();
+builder.Services.AddScoped<IInventoryService, InventoryService>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
